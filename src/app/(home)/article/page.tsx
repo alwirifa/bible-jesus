1831e56ac0f024371d9d34891d23b639771637a2
Alwi <alwirifa7@gmail.com>
2024-06-19 23:53:14 +0700

    second commit

--- src/app/(home)/article/page.tsx
+++ src/app/(home)/article/page.tsx
@@ -33,9 +33,9 @@ const Page: React.FC<Props> = () => {
     <div className="font-poppins">
       <Container>
         {articleData.map((article, index) => (
-          <div key={index} className=" flex gap-10 mb-10">
-            <div className="flex w-[685px] ">
-              <div className="relative h-[400px] w-[550px]">
+          <div key={index} className="flex flex-col lg:flex-row gap-6 lg:gap-10 mb-10">
+            <div className="flex lg:w-[550px] xl:w-[650px] 2xl:w-[750px]">
+              <div className="relative h-[200px] sm:h-[300px] md:h-[350px] lg:h-[400px] lg:w-[550px] w-full">
                 <Image
                   src={article.image}
                   alt={article.image}
@@ -44,13 +44,15 @@ const Page: React.FC<Props> = () => {
                 />
               </div>
             </div>
-            <div className="w-full flex gap-4 flex-col relative">
-              <p className="text-2xl font-[300] mt-8">{article.date}</p>
-              <h2 className="text-[42px] font-semibold mb-2">
+            <div className="w-full flex flex-col gap-4 relative">
+              <p className="text-lg sm:text-xl md:text-2xl font-[300] mt-4 lg:mt-8">{article.date}</p>
+              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-[42px] font-semibold mb-2">
                 {article.title}
               </h2>
-              <p className="text-xl font-[300]">{article.subtitle}</p>
-              <div className="absolute bottom-0 left-0 px-16 py-3 text-white bg-black text-xl font-[400] max-w-max">Read More</div>
+              <p className="text-base sm:text-lg md:text-xl font-[300]">{article.subtitle}</p>
+              <div className="flex md:absolute bottom-0 left-0 px-8 sm:px-10 md:px-12 lg:px-16 py-2 sm:py-3 text-white bg-black text-base sm:text-lg md:text-xl font-[400] max-w-max">
+                Read More
+              </div>
             </div>
           </div>
         ))}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-black text-white  w-full font-dm_sans">
+    <footer className="bg-black text-white w-full font-dm_sans">
       <Container>
-        <div className="pt-28 pb-36 flex gap-32 px-20">
-          <div className="space-y-8 ">
-          <Image src={"/images/MaisonLogoBlack.svg"} alt="logo-black" height={80} width={180} />
-            <p className="text-xl text-grey">
-              The Scents of Flower And Sweet <br/> Memory of Butterflies
+        <div className="pt-14 md:pt-28 pb-16 md:pb-36 flex flex-col lg:flex-row gap-12 lg:gap-32 px-4 sm:px-10 md:px-20">
+          <div className="space-y-8">
+            <Image src={"/images/MaisonLogoBlack.svg"} alt="logo-black" height={80} width={180} />
+            <p className="text-lg sm:text-xl text-grey">
+              The Scents of Flower And Sweet <br /> Memory of Butterflies
             </p>
             <div className="flex space-x-4 mt-4">
               <a href="#" className="text-white hover:text-gray-400">
@@ -30,9 +30,9 @@ const Footer: React.FC = () => {
               </a>
             </div>
           </div>
-          <div className="flex flex-col gap-10">
-            <h3 className="text-[22px] font-[700]">Navigation</h3>
-            <ul className="text-grey text-xl capitalize space-y-4">
+          <div className="flex flex-col gap-6 lg:gap-10">
+            <h3 className="text-xl lg:text-[22px] font-[700]">Navigation</h3>
+            <ul className="text-grey text-lg sm:text-xl capitalize space-y-4">
               <li>
                 <a href="#" className="hover:text-gray-400">
                   Catalogue
@@ -50,9 +50,9 @@ const Footer: React.FC = () => {
               </li>
             </ul>
           </div>
-          <div className="flex flex-col gap-10">
-            <h3 className="text-[22px] font-[700]">Contact us</h3>
-            <ul className="text-grey text-xl capitalize space-y-4">
+          <div className="flex flex-col gap-6 lg:gap-10">
+            <h3 className="text-xl lg:text-[22px] font-[700]">Contact us</h3>
+            <ul className="text-grey text-lg sm:text-xl capitalize space-y-4">
               <li>
                 <a
                   href="mailto:hi@maisondeophelia.com"
@@ -73,21 +73,20 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        <div className="px-20">
-
-        <div className="flex justify-between items-center w-full text-xl font-[400] text-grey border-t border-white pt-6 px-4">
-          <p>Copyright &copy; 2024 Maison D' Ophelia</p>
-          <div className="flex gap-4">
-            <p> All Rights Reserved | </p>
-            <a href="#" className="hover:text-gray-400">
-              Terms and Conditions
-            </a>{" "}
-            |{" "}
-            <a href="#" className="hover:text-gray-400">
-              Privacy Policy
-            </a>
+        <div className="px-4 sm:px-10 md:px-20">
+          <div className="flex flex-col lg:flex-row justify-between items-center w-full text-lg sm:text-xl font-[400] text-grey border-t border-white pt-6 lg:pt-6 px-4">
+            <p className="text-center lg:text-left">Copyright &copy; 2024 Maison D&apos; Ophelia</p>
+            <div className="flex gap-4 mt-4 lg:mt-0">
+              <p> All Rights Reserved | </p>
+              <a href="#" className="hover:text-gray-400">
+                Terms and Conditions
+              </a>{" "}
+              |{" "}
+              <a href="#" className="hover:text-gray-400">
+                Privacy Policy
+              </a>
+            </div>
           </div>
-        </div>
         </div>
       </Container>
     </footer>
